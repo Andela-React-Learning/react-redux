953f3e1244f13404c522c8b4a5ae5d55eeb344ec
Alexotieno1717 <alexotieno900@gmail.com>
2023-09-06 09:27:12 +0300

    :sparkles: [feat]: created components home, about and courses

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import "./index.css";
 
-function Hi() {
-  return <div>Hi</div>;
-}
-
-// export default Hi
-render(<Hi />, document.getElementById("app"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
